a868f1f59650df2d28a1c73a72ac652346dcc985
Teresa <107971753+TMortara@users.noreply.github.com>
2022-12-22 18:29:16 -0600

    add array of objects, setup require, and start functions

--- index.js
+++ index.js
@@ -1,13 +1,69 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {   type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    {   type: "input",
+        message: "What is the description of your project?",
+        name: "description"
+    },
+    {   type: "input",
+        message: "Please list any installation steps for your project:",
+        name: "installation"
+    },
+    {   type: "input",
+        message: "How do you navigate your project?",
+        name: "usage"
+    },
+    {   type: "input",
+        message: "List all contributors to your project",
+        name: "contributors"
+    },
+    {   type: "input",
+        message: "List any test instructions for your project:",
+        name: "test"
+    },
+    {   type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {   type: "checkbox",
+        message: "Choose your license:",
+        name: "license",
+        choices: ["MIT", "Apache 2.0", "Perl", "None"]
+    },
+    {   type: "input",
+        message: "What is your email address?",
+        name: "email"
+        // validate:
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile("README.md",generateMarkdown(responses), err =>{
+        if (err){
+            console.log(err)
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then(respones => {
+         
+        
+    })
+
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,49 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license !== "None") {
+    return `![GitHub License](https://img.shields.io/badge/License-${license}-yellow.svg)`;
+  } 
+  return "";
+}
+
+//MIT: [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+//Apache: [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+//Perl: [![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license !== "None") {
+    return 
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license !== "None") {
+    return `## License
+    
+    This application is under the ${license} license.`
+  } return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
+${renderLicenseBadge(data.license)}
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+
 
 `;
 }
